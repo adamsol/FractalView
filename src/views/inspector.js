f0e11c0bfad360da73cbea4603d8981983dfbd96
Adam Sołtysik <adam27.sol@gmail.com>
2018-12-16 22:10:13 +0100

    Lighting selector

--- src/views/inspector.js
+++ src/views/inspector.js
@@ -1,35 +1,59 @@
 
 function InspectorView(container, state)
 {
-	this.container = container.getElement().empty();
+	let self = this;
+	self.container = container.getElement().empty();
 
-	let select = $('<select>', {id: 'fractal'}).appendTo(this.container).append($('<option>', {text: '-- select --'}));
-	fs.readdir('src/renderers/fractal', (err, files) => {
-		for (let name of files) {
-			$('<option>', {value: name, text: name}).appendTo(select);
-		}
-	});
-	select.on('change', function() {
-		let vertex = $.get('renderers/base.vert');
-		let fragment = $.get('renderers/base.frag');
-		let lighting = $.get('renderers/lighting/lambert-shadow.frag');
-		let fractal = $.get('renderers/fractal/{}'.format($(this).val()));
-		$.when(vertex, fragment, lighting, fractal).done(function(vertex, fragment, lighting, fractal) {
-			if (mesh) {
-				scene.remove(mesh);
-			}
-			mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2.0, 2.0), null);
-			mesh.material = new THREE.RawShaderMaterial({
-				uniforms: {
-					CX: {value: 2.0},
-					screenRes: {},
-					cameraPos: {},
-					cameraDir: {},
-				},
-				vertexShader: vertex[0],
-				fragmentShader: fragment[0].format({lighting: lighting[0], fractal: fractal[0]}),
-			});
-			scene.add(mesh);
-		});
-	});
+	let fractal_select = $('<select>', {'class': 'fractal'}).appendTo(self.container);
+	for (let name of fs.readdirSync('src/renderers/fractal')) {
+		$('<option>', {value: name, text: path.basename(name, '.glsl')}).appendTo(fractal_select);
+	};
+	fractal_select.on('change', {self: self}, self.onChange);
+
+	let lighting_select = $('<select>', {'class': 'lighting'}).appendTo(self.container);
+	for (let name of fs.readdirSync('src/renderers/lighting')) {
+		$('<option>', {value: name, text: path.basename(name, '.glsl')}).appendTo(lighting_select);
+	};
+	lighting_select.on('change', {self: self}, self.onChange);
+
+	self.update();
 }
+
+InspectorView.prototype.onChange = function(event)
+{
+	let self = event.data.self;
+	self.update();
+};
+
+InspectorView.prototype.update = function()
+{
+	let self = this;
+	let fractal = self.container.find('.fractal').val();
+	let lighting = self.container.find('.lighting').val();
+	if (!fractal || !lighting) {
+		return;
+	}
+	let promises = [
+		$.get('renderers/base.vert'),
+		$.get('renderers/base.frag'),
+		$.get('renderers/fractal/{}'.format(fractal)),
+		$.get('renderers/lighting/{}'.format(lighting)),
+	];
+	$.when(...promises).done(function(vertex, fragment, fractal, lighting) {
+		if (mesh) {
+			scene.remove(mesh);
+		}
+		mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2.0, 2.0), null);
+		mesh.material = new THREE.RawShaderMaterial({
+			uniforms: {
+				CX: {value: 2.0},
+				screenRes: {},
+				cameraPos: {},
+				cameraDir: {},
+			},
+			vertexShader: vertex[0],
+			fragmentShader: fragment[0].format({fractal: fractal[0], lighting: lighting[0]}),
+		});
+		scene.add(mesh);
+	});
+};
